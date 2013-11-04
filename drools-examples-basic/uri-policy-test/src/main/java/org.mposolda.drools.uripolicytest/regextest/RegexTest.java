367398dee3fb80bdf1f76fbc000674ccae97ae7d
mposolda <mposolda@gmail.com>
2013-11-04 16:43:55 +0100

    Added some regex examples

---
+++ drools-examples-basic/uri-policy-test/src/main/java/org.mposolda.drools.uripolicytest/regextest/RegexTest.java
@@ -0,0 +1,24 @@
+package org.mposolda.drools.uripolicytest.regextest;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author <a href="mailto:mposolda@redhat.com">Marek Posolda</a>
+ */
+public class RegexTest {
+
+    public static void main(String[] args) {
+        test("^/neco/(.*)/ml\\?ok$", "/neco/spmething/koleso/ml?ok");
+
+        test("^/neco/(neco1|neco2)/neco3", "/neco/neco2/neco3");
+    }
+
+    private static void test(String regex, String textToTest) {
+        Pattern p = Pattern.compile(regex);
+        Matcher m = p.matcher(textToTest);
+        while (m.find()) {
+            System.out.println("Got it! START: " + m.start() + ", END: " + m.end() + ", GROUP: " + m.group());
+        }
+    }
+}
