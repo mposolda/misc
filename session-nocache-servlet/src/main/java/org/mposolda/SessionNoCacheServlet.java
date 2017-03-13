7f72c288f7353e942452d3e847440cc8ac347bc0
mposolda <mposolda@gmail.com>
2017-03-13 21:58:34 +0100

    Code refactoring

--- session-nocache-servlet/src/main/java/org/mposolda/SessionNoCacheServlet.java
+++ session-nocache-servlet/src/main/java/org/mposolda/SessionNoCacheServlet.java
@@ -13,10 +13,30 @@ import javax.servlet.http.HttpSession;
  */
 public class SessionNoCacheServlet extends HttpServlet {
 
+    private enum CodeCheck {
+        VALID, INVALID, EXPIRED
+    }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("POST received!");
+
+        String queryString = req.getQueryString();
+        CodeCheck codeCheck = checkCode(queryString, req.getSession());
+
+        // Restart flow from scratch
+        if (codeCheck == CodeCheck.INVALID) {
+            System.out.println("Code invalid. Restarting from Scratch!");
+            req.getSession().invalidate();
+            String appUri = req.getScheme() + "://localhost:" + req.getLocalPort() + "/session-nocache-servlet/app";
+            resp.sendRedirect(appUri);
+            return;
+        } else if (codeCheck == CodeCheck.EXPIRED) {
+            System.out.println("Code expired. Just redirecting to get");
+            String getUri = req.getScheme() + "://localhost:" + req.getLocalPort() + "/session-nocache-servlet/s";
+            resp.sendRedirect(getUri);
+            return;
+        }
 
         String username = req.getParameter("username");
         System.out.println("username from parameter: " + username);
@@ -24,15 +44,40 @@ public class SessionNoCacheServlet extends HttpServlet {
         HttpSession session = req.getSession();
         session.setAttribute("username", username);
 
-        //render(req, resp);
-        resp.sendRedirect(req.getRequestURL().toString());
+        render(req, resp);
+        //resp.sendRedirect(req.getRequestURL().toString());
     }
 
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("GET received!");
+        computeNewCode(req.getSession());
         render(req, resp);
+    }
+
+
+    private CodeCheck checkCode(String queryString, HttpSession session) {
+        Integer sessionCode = getCurrentCode(session);
+
+        if (queryString == null) {
+            return CodeCheck.INVALID;
+        }
+
+        int index =  (queryString.indexOf('='));
+        if (index == -1) {
+            return CodeCheck.INVALID;
+        }
+
+        Integer paramCode = Integer.parseInt(queryString.substring(index + 1));
+        System.out.println("sessionCode=" + sessionCode + ", paramCode=" + paramCode);
+        if (sessionCode == paramCode) {
+            return CodeCheck.VALID;
+        } else if (sessionCode > paramCode) {
+            return CodeCheck.EXPIRED;
+        } else {
+            return CodeCheck.INVALID;
+        }
     }
 
 
@@ -43,7 +88,14 @@ public class SessionNoCacheServlet extends HttpServlet {
         String username = (String) session.getAttribute("username");
         String actionURI = req.getRequestURL().toString();
 
+        Integer newCode = computeNewCode(session);
+        actionURI = actionURI + "?code=" + newCode;
+
         resp.setHeader("Content-Type", "text/html");
+
+        // no back-button
+        //resp.setHeader("Cache-Control", "no-store, must-revalidate, max-age=0");
+
         String responseString = renderForm(actionURI, username);
         resp.getWriter().println(responseString);
         resp.getWriter().flush();
@@ -60,6 +112,18 @@ public class SessionNoCacheServlet extends HttpServlet {
 
         builder.append("</BODY></HTML>");
         return builder.toString();
+    }
+
+
+    private Integer computeNewCode(HttpSession session) {
+        Integer code = (Integer) session.getAttribute("code");
+        code = code==null ? 1 : code+1;
+        session.setAttribute("code", code);
+        return code;
+    }
+
+    private Integer getCurrentCode(HttpSession session) {
+        return (Integer) session.getAttribute("code");
     }
 
 }
