afa8aefc24bdd46ff5a4f529493c6d2f89bf86d2
mposolda <mposolda@gmail.com>
2017-09-29 21:35:22 +0200

    Deflate testing

--- jwe-encryption-nimbus/src/main/java/org/mposolda/nimbus/NimbusExample.java
+++ jwe-encryption-nimbus/src/main/java/org/mposolda/nimbus/NimbusExample.java
@@ -1,5 +1,6 @@
 package org.mposolda.nimbus;
 
+import com.nimbusds.jose.CompressionAlgorithm;
 import com.nimbusds.jose.EncryptionMethod;
 import com.nimbusds.jose.JWEAlgorithm;
 import com.nimbusds.jose.JWEHeader;
@@ -7,6 +8,8 @@ import com.nimbusds.jose.JWEObject;
 import com.nimbusds.jose.Payload;
 import com.nimbusds.jose.crypto.DirectDecrypter;
 import com.nimbusds.jose.crypto.DirectEncrypter;
+import com.nimbusds.jose.util.Base64URL;
+import com.nimbusds.jose.util.DeflateUtils;
 
 /**
  * @author <a href="mailto:mposolda@redhat.com">Marek Posolda</a>
@@ -14,27 +17,64 @@ import com.nimbusds.jose.crypto.DirectEncrypter;
 public class NimbusExample {
 
     public static void main(String[] args) throws Exception {
-        JWEHeader header = new JWEHeader(JWEAlgorithm.DIR, EncryptionMethod.A128CBC_HS256);
-        JWEObject jweObj = new JWEObject(header, new Payload("Hello world! How are you man? I hope you are fine. This is some quite a long text, which is much longer than just simple 'Hello World'"));
+        long start = System.currentTimeMillis();
+        for (int i=0 ; i<50000 ; i++) {
+            JWEHeader header = new JWEHeader(JWEAlgorithm.DIR, EncryptionMethod.A128CBC_HS256);
+            //header.setCompressionAlgorithm(CompressionAlgorithm.DEF);
 
-        byte[] key128 = new byte[] { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 13, 14, 15, 16 };
-        byte[] key256 = new byte[] { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 13, 14, 15, 16, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 };
-        //byte[] key = key128;
-        byte[] key = key256;
-
-        DirectEncrypter encrypter = new DirectEncrypter(key);
-
-        // You can now use the encrypter on one or more JWE objects
-        // that you wish to secure
-        jweObj.encrypt(encrypter);
-        String jweString = jweObj.serialize();
-
-        System.out.println(jweString);
+            JWEObject jweObj = new JWEObject(header, new Payload("{\"jti\":\"09c71bb4-c281-41e9-85d3-a17c9b0b0965\",\"exp\":1506711874,\"nbf\":0,\"iat\":1506711814,\"azp\":\"b7ecce76-6324-4482-b992-15576acb8d98\",\"uss\":\"aab3cb03-0a4d-4303-bed8-43498f13b203\"}"));
 
 
-        DirectDecrypter decrypter = new DirectDecrypter(key);
-        jweObj = JWEObject.parse(jweString);
-        jweObj.decrypt(decrypter);
-        System.out.println("Decrypted payload: " + jweObj.getPayload().toString());
+            byte[] key128 = new byte[]{10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 13, 14, 15, 16};
+            byte[] key256 = new byte[]{10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 13, 14, 15, 16, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
+            byte[] key512 = new byte[]{10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 13, 14, 15, 16, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 13, 14, 15, 16, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
+            //byte[] key = key128;
+            byte[] key = key256;
+            //byte[] key = key512;
+
+            DirectEncrypter encrypter = new DirectEncrypter(key);
+
+            // You can now use the encrypter on one or more JWE objects
+            // that you wish to secure
+            jweObj.encrypt(encrypter);
+            String jweString = jweObj.serialize();
+
+            //System.out.println(jweString);
+            //System.out.println("jweString.length(): " + jweString.length());
+
+
+            DirectDecrypter decrypter = new DirectDecrypter(key);
+            jweObj = JWEObject.parse(jweString);
+//        jweObj = JWEObject.parse("eyJhbGciOiJkaXIiLCJlbmMiOiJBMTI4Q0JDLUhTMjU2In0..M2r_deQ_NT_Gvz_eKipkXg.rRgk1fmfdSR3LHJIOayHwxl82-ACiBJLgaPVJNmTz3gW-o_-fsQD2bIhx102EQN55J2g7xFtDJE6J0KpUeuI_kXYYJkssQNHcrbDM52tsacuUrNYN17xYfeRNrdNn8g9kv7HLfG89bcSocks1uhKLEeUNCQB6uIIhdddttpwjKMXme1J4hvFFn63bzNt2im3.z_uyUjqW-xcLD9iWZiUxXQ");
+
+            jweObj.decrypt(decrypter);
+            //System.out.println("Decrypted payload: " + jweObj.getPayload().toString());
+        }
+        long took = System.currentTimeMillis() - start;
+        System.out.println("took: " + took + " ms");
+
+        deflateTest();
+    }
+
+
+    public static void deflateTest() throws Exception {
+
+        final String text = "{\"jti\":\"09c71bb4-c281-41e9-85d3-a17c9b0b0965\",\"exp\":1506711874,\"nbf\":0,\"iat\":1506711814,\"azp\":\"b7ecce76-6324-4482-b992-15576acb8d98\",\"uss\":\"aab3cb03-0a4d-4303-bed8-43498f13b203\"}";
+        final byte[] textBytes = text.getBytes("UTF-8");
+        String base64Text = Base64URL.encode(textBytes).toString();
+
+        byte[] compressed = DeflateUtils.compress(textBytes);
+        String base64TextCompressed = Base64URL.encode(compressed).toString();
+
+        byte[] textBytesDecompressed = DeflateUtils.decompress(compressed);
+        String textDecompressed = new String(textBytesDecompressed, "UTF-8");
+
+        System.out.println("byte length check. textBytes.length: " + textBytes.length + ", textBytesDecompressed.length: " + textBytesDecompressed.length + ", compressed.length: " + compressed.length);
+
+        System.out.println("Decompressed text: " + textDecompressed);
+
+        System.out.println("Base64Text.length: " + base64Text.length() + ", Base64TextCompressed.length: " + base64TextCompressed.length());
+
+
     }
 }
--- jwe-example/src/main/java/org/mposolda/JWEDirectEncryptionTest.java
+++ jwe-example/src/main/java/org/mposolda/JWEDirectEncryptionTest.java
@@ -2,6 +2,7 @@ package org.mposolda;
 
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
+import java.security.MessageDigest;
 import java.security.Security;
 import java.security.spec.AlgorithmParameterSpec;
 import java.util.Arrays;
@@ -29,12 +30,22 @@ public class JWEDirectEncryptionTest {
         byte[] hmacSHA256key = new byte[] { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 13, 14, 15, 16 };
         byte[] aes128key =  new byte[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 };
 
-        String initializationVector = "F6__fHjInfbBYPLGsuNN7A";
+        //byte[] hmacSHA512key = new byte[] { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 13, 14, 15, 16, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 };
+        //byte[] aes256key =  new byte[] { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 13, 14, 15, 16, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 };
+
+        String initializationVector = "85kfvBWwrkfyZ7Hb26DB3A";
 
         SecretKeySpec aesKeySpec = new SecretKeySpec(aes128key, "AES");
         SecretKeySpec hmacKeySpec = new SecretKeySpec(hmacSHA256key, "HMACSHA2");
 
-        String encrypted = encrypt(payload, initializationVector, aesKeySpec, hmacKeySpec);
+        long start = System.currentTimeMillis();
+        for (int i=0 ; i<1 ; i++) {
+            //String encrypted = encrypt(payload, initializationVector, aesKeySpec, hmacKeySpec);
+            String encrypted = "eyJlbmMiOiJBMTI4Q0JDLUhTMjU2IiwiYWxnIjoiZGlyIn0..JRHAnPiPbNhcel2zHmHc4A.u-uXO5PAcBLK1Q7f0MP6i_tIzBpDxUChVyNvdbf6te-bDlq96LvJqvPoesxSjnScVxGfO3jT8wQmUjc7k7xZ0vG_ssLJtjaizhuoME_d2EWRlHlJLd2tHIJFUZ7gK0f4SGgN4lnCMylypOzy4S0x9-m4sBkU9-982cQLEqbs6NLK7Vo52JvF39YcVJZqFajQ.xAEsLduTkL7HZHnXokc-JQ";
+            String decrypted = decrypt(encrypted, aesKeySpec, hmacKeySpec);
+        }
+        long took = System.currentTimeMillis() - start;
+        System.out.println(took + " ms");
         //String decrypted = decrypt(encrypted);
 
         //System.out.println(decrypted);
@@ -42,12 +53,13 @@ public class JWEDirectEncryptionTest {
 
 
     private static String encrypt(String payload, String initializationVector, SecretKeySpec aesKeySpec, SecretKeySpec hmacKeySpec) throws Exception {
+        // FIXME
         //String header = "{\"alg\":\"dir\",\"enc\":\"A128CBC-HS256\"}";
         String header = "{\"enc\":\"A128CBC-HS256\",\"alg\":\"dir\"}";
-        System.out.println("Header: " + header);
+        //System.out.println("Header: " + header);
 
         String encodedHeader = Base64Url.encode(header.getBytes("UTF-8"));
-        System.out.println("Encoded header: " + encodedHeader);
+        //System.out.println("Encoded header: " + encodedHeader);
 
         byte[] payloadBytes = payload.getBytes("UTF-8");
 
@@ -60,7 +72,7 @@ public class JWEDirectEncryptionTest {
 
         // A.3.5
         byte[] aadBytes = encodedHeader.getBytes("UTF-8");
-        System.out.println(aadBytes);
+       // System.out.println(aadBytes);
 
         // B.3
         ByteBuffer b = ByteBuffer.allocate(4);
@@ -80,33 +92,92 @@ public class JWEDirectEncryptionTest {
 
 
         // B.6
+        // FIXME
         Mac macImpl = Mac.getInstance("HMACSHA256");
         macImpl.init(hmacKeySpec);
         macImpl.update(concatenatedHmacInput);
         byte[] macEncoded =  macImpl.doFinal();
 
-        byte[] aadOutput = Arrays.copyOf(macEncoded, 16);
+        // FIXME
+        byte[] aadOutput = Arrays.copyOf(macEncoded, 32);
 
         // A.3.6 - continue
         String cipherText = Base64Url.encode(encryptedBytes);
-        System.out.println("cipherText: " + cipherText);
+        //System.out.println("cipherText: " + cipherText);
 
         String aadOutputText= Base64Url.encode(aadOutput);
-        System.out.println("aadOutputText: " + aadOutputText);
+        //System.out.println("aadOutputText: " + aadOutputText);
 
         String jwe = encodedHeader + ".."
                 + initializationVector + "."
                 + cipherText + "."
                 + aadOutputText;
 
-        System.out.println("JWE: " + jwe);
+        //System.out.println("JWE: " + jwe);
 
         return jwe;
 
     }
 
 
-//    private static String decrypt(String encrypted) {
-//
-//    }
+
+
+
+    private static String decrypt(String encrypted, SecretKeySpec aesKeySpec, SecretKeySpec hmacKeySpec) throws Exception {
+        String[] parts = encrypted.split("\\.");
+        String header = parts[0];
+        String iv = parts[2];
+        String cipherText = parts[3];
+        String at = parts[4];
+
+        // Check the key lengths TODO
+
+        // Compare the hash
+        byte[] aadBytes = header.getBytes("UTF-8");
+        byte[] ivBytes = Base64Url.decode(iv);
+
+        // B.3
+        ByteBuffer b = ByteBuffer.allocate(4);
+        b.order(ByteOrder.BIG_ENDIAN); // optional, the initial order of a byte buffer is always BIG_ENDIAN.
+        int aadLengthInBits = aadBytes.length * 8;
+        b.putInt(aadLengthInBits);
+        byte[] result1 = b.array();
+        byte[] aadBigEndian = new byte[8];
+        System.arraycopy(result1, 0, aadBigEndian, 4, 4);
+
+        byte[] encryptedBytes = Base64Url.decode(cipherText);
+
+        // B.5
+        byte[] concatenatedHmacInput = new byte[aadBytes.length + ivBytes.length + encryptedBytes.length + aadBigEndian.length];
+        System.arraycopy(aadBytes, 0, concatenatedHmacInput, 0, aadBytes.length);
+        System.arraycopy(ivBytes, 0, concatenatedHmacInput, aadBytes.length, ivBytes.length );
+        System.arraycopy(encryptedBytes, 0, concatenatedHmacInput, aadBytes.length + ivBytes.length , encryptedBytes.length);
+        System.arraycopy(aadBigEndian, 0, concatenatedHmacInput, aadBytes.length + ivBytes.length + encryptedBytes.length, aadBigEndian.length);
+
+        // B.6
+        // FIXME
+        Mac macImpl = Mac.getInstance("HMACSHA256");
+        macImpl.init(hmacKeySpec);
+        macImpl.update(concatenatedHmacInput);
+        byte[] macEncoded =  macImpl.doFinal();
+
+        // FIXME
+        byte[] aadOutput = Arrays.copyOf(macEncoded, 16);
+        byte[] atBytes = Base64Url.decode(at);
+        boolean digitsEqual = MessageDigest.isEqual(aadOutput, atBytes);
+
+        if (!digitsEqual) {
+            // TODO
+            throw new IllegalArgumentException("Signature validations failed");
+        }
+
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS7Padding", "BC");
+        AlgorithmParameterSpec ivParamSpec = new IvParameterSpec(ivBytes);
+        cipher.init(Cipher.DECRYPT_MODE, aesKeySpec, ivParamSpec);
+        byte[] payloadBytes = cipher.doFinal(encryptedBytes);
+
+        String payload = new String(payloadBytes, "UTF-8");
+        System.out.println("P: " + payload);
+        return payload;
+    }
 }
